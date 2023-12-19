62e0b774762dd2b2858fd1fb10d6843f6a2fe286
锐源 <1370164516@qq.com>
2023-12-19 17:36:30 +0800

    Update App.vue

--- vuejs-2.0-cli/src/App.vue
+++ vuejs-2.0-cli/src/App.vue
@@ -11,6 +11,7 @@
 import HelloWorld from './components/HelloWorld'
 import Todos from "./components/Todos.vue";
 import TodoForm from "./components/TodoForm.vue";
+// import axios from "axios";
 
 export default {
   name: 'App',
@@ -27,13 +28,13 @@ export default {
     // },
   },
 
-  mounted() {
-    this.axios.get('http://vue2.test/api/todos').then(
-      response => {
-        console.log(response.data);
-      }
-    );
-  },
+  // mounted() {
+  //   axios.get('http://vue2.test/api/todos').then(
+  //     response => {
+  //       console.log(response.data);
+  //     }
+  //   );
+  // },
   computed:{
     todosCount(){
       return this.todos.length;
